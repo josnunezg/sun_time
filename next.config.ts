431a64840ee179e1f4bdbc9565b799cc6d58085e
Jose Nunez <josnunezg@gmail.com>
2025-06-01 00:14:11 -0400

    add shadcn ui

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    SUNRISE_SUNSET_API_URL: process.env.SUNRISE_SUNSET_API_URL,
+    SUNRISE_SUNSET_API_KEY: process.env.SUNRISE_SUNSET_API_KEY,
+    NEXT_PUBLIC_INTERNAL_HEADER_KEY: process.env.NEXT_PUBLIC_INTERNAL_HEADER_KEY
+  }
 };
 
 export default nextConfig;
